b35caf5572b9f00ce89255e5d832e7b6a6fec024
Michal Vašíček <michal.vasicek@tapmates.com>
2010-11-05 20:47:29 +0100

    Initial commit

---
+++ src/CouchSourfing/iPhone/AppDelegate_iPhone.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate_iPhone.h
+//  CouchSourfing
+//
+//  Created by Michal Vašíček on 11/5/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate_iPhone : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
