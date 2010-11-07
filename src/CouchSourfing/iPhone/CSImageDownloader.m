b9478cdcf07fc7538eb438ff457967f8a3ea5152
Michal Vašíček <michal.vasicek@tapmates.com>
2010-11-07 17:53:25 +0100

    Podpora pro proporcionalni resize obrazku

--- src/CouchSourfing/iPhone/CSImageDownloader.m
+++ src/CouchSourfing/iPhone/CSImageDownloader.m
@@ -13,6 +13,8 @@
 @property (nonatomic, retain) NSMutableData *data;
 @property (nonatomic, retain) NSURLConnection *connection;
 
+- (UIImage*)scaleToSize:(CGSize)size image:(UIImage *)image;
+
 @end
 
 
@@ -39,7 +41,9 @@
 }
 
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection {
-    UIImage *image = [UIImage imageWithData:self.data];
+    
+    UIImage *image = [self scaleToSize:CGSizeMake(50, 50) image:[UIImage imageWithData:self.data]];
+    
     if ([self.delegate respondsToSelector:@selector(imageDownloader:didDownloadImage:forPosition:)]) {
         [self.delegate imageDownloader:self didDownloadImage:image forPosition:_position];
     }
@@ -55,5 +59,61 @@
     self.connection = nil;
     [super dealloc];
 }
+
+#pragma mark Private methods
+
+- (UIImage*)scaleToSize:(CGSize)maxSize image:(UIImage *)image {
+    CGImageRef cgImage = image.CGImage;
+    
+    size_t originalWidth = CGImageGetWidth(cgImage);
+    size_t originalHeight = CGImageGetHeight(cgImage);
+    
+    CGFloat widthRatio = maxSize.width / originalWidth;
+    CGFloat heightRatio = maxSize.height / originalHeight;
+    
+    CGFloat newWidth;
+    CGFloat newHeight;
+    
+    if (widthRatio < 1) {
+        newWidth = originalWidth * widthRatio;
+        newHeight = originalHeight * widthRatio;
+        
+        CGFloat newHeightRatio = maxSize.height / newHeight;
+        
+        if (newHeightRatio < 1) {
+            newWidth *= newHeightRatio;
+            newHeight *= newHeightRatio;
+        }
+    } else if (heightRatio < 1) {
+        newWidth = originalWidth * heightRatio;
+        newHeight = originalHeight * heightRatio;
+        
+        CGFloat newWidthRatio = maxSize.width / newWidth;
+        
+        if (newWidth < 1) {
+            newWidth *= newWidthRatio;
+            newHeight *= newWidthRatio;
+        }
+    } else {
+        newWidth = originalWidth;
+        newHeight = originalHeight;
+    }
+
+    CGSize size = CGSizeMake(newWidth, newHeight);
+	UIGraphicsBeginImageContext(size);
+	
+	CGContextRef context = UIGraphicsGetCurrentContext();
+	CGContextTranslateCTM(context, 0.0, size.height);
+	CGContextScaleCTM(context, 1.0, -1.0);
+	
+	CGContextDrawImage(context, CGRectMake(0.0f, 0.0f, size.width, size.height), image.CGImage);
+	
+	UIImage* scaledImage = UIGraphicsGetImageFromCurrentImageContext();
+	
+	UIGraphicsEndImageContext();
+	
+	return scaledImage;
+}
+
 
 @end
