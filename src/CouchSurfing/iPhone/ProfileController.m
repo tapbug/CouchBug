12303761947b4b98b295bbd8a7dc5ca4bfc05a6b
Michal Vašíček <michal.vasicek@mvas.cz>
2011-06-18 08:44:24 +0200

    Otocny profile controller

--- src/CouchSurfing/iPhone/ProfileController.m
+++ src/CouchSurfing/iPhone/ProfileController.m
@@ -113,10 +113,8 @@
     // e.g. self.myOutlet = nil;
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return YES;
 }
 
 #pragma Mark ProfileRequestDelegate methods
